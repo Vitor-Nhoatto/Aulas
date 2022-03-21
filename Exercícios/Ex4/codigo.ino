55b9aa21f721ea6e3a5422bb13f7b53d3a65e4fc
Vitor Henrique Nhoatto Bagattini <78737742+Vitor-Nhoatto@users.noreply.github.com>
2022-03-21 09:29:29 -0300

    Create codigo.ino

---
+++ Exercícios/Ex4/codigo.ino
@@ -0,0 +1,21 @@
+const int led = 2;
+const int botao = 3;
+
+int var;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  pinMode(botao, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  var = digitalRead(botao);
+  
+   if(var == HIGH)
+   {
+     digitalWrite(led, HIGH);
+   }
+}
